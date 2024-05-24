c36c463272b040f76a8c18d56f29cf5fbde792fa
BJM0203 <70325966+BJM0203@users.noreply.github.com>
2024-05-24 22:32:36 +0900

    Add files via upload

---
+++ 0521/scanfex.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int numinput;
+    char charinput;
+    scanf("%d", &numinput);
+    printf("%d\n", numinput);
+    scanf(" %c", &charinput);
+    printf("%c\n", charinput);
+    return 0;
+
